dccb528d7154cb68157f0f17d8d4db69b0bf0ae0
Mario Chois <mario.chois.navarro@gmail.com>
2022-01-08 21:20:35 -0500

    MediaPlayer: Add Play button with play music functionality

--- app/src/main/java/com/ravnnerdery/musicplayer/MainActivity.kt
+++ app/src/main/java/com/ravnnerdery/musicplayer/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.ravnnerdery.musicplayer
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var play_btn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        play_btn = findViewById(R.id.play_btn)
+        var mediaSound = MediaPlayer.create(this, R.raw.backinblack)
+        play_btn.setOnClickListener{
+            mediaSound.start()
+        }
     }
 }
