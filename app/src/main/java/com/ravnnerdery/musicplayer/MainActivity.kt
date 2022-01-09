26e41e0691a5037bfea693add7e65706012fb46e
Mario Chois <mario.chois.navarro@gmail.com>
2022-01-08 22:34:33 -0500

    Layout: Add circular action controller for media player

--- app/src/main/java/com/ravnnerdery/musicplayer/MainActivity.kt
+++ app/src/main/java/com/ravnnerdery/musicplayer/MainActivity.kt
@@ -7,13 +7,24 @@ import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     lateinit var play_btn: Button
+    var playing: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         play_btn = findViewById(R.id.play_btn)
         var mediaSound = MediaPlayer.create(this, R.raw.backinblack)
         play_btn.setOnClickListener{
-            mediaSound.start()
+            if (playing) {
+                mediaSound.pause()
+                playing = false
+            } else {
+                mediaSound.start()
+                playing = true
+            }
+
         }
+
+
+
     }
 }
