e44360032731f0f93ae2aa0e3d08cfc7867a4105
Mario Chois <mario.chois.navarro@gmail.com>
2022-01-11 13:32:14 -0500

    Music: Moove music to assets/music folder

--- app/src/main/java/com/ravnnerdery/musicplayer/MusicListFragment.kt
+++ app/src/main/java/com/ravnnerdery/musicplayer/MusicListFragment.kt
@@ -1,5 +1,6 @@
 package com.ravnnerdery.musicplayer
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,13 +8,21 @@ import android.view.View
 import android.view.ViewGroup
 
 class MusicListFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        var mediaSound = MediaPlayer.create(context,R.raw.backinblack)
+        val allMedia = assets.
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val binding = inflater.inflate(R.layout.fragment_music_list, container, false)
-        return binding
+        return inflater.inflate(R.layout.fragment_music_list, container, false)
     }
+
 
 }
