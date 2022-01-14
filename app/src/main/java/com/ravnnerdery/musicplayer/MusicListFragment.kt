bb5fff87199d38ae92f8eefeb27e4fb9626934b4
hunter4466 <mario.chois.navarro@gmail.com>
2022-01-13 21:31:09 -0500

    AlbumPic: Add dynamic album picture to nowPlaying fragment depending on now Playin music

--- app/src/main/java/com/ravnnerdery/musicplayer/MusicListFragment.kt
+++ app/src/main/java/com/ravnnerdery/musicplayer/MusicListFragment.kt
@@ -13,7 +13,6 @@ import androidx.navigation.findNavController
 
 open class MusicListFragment : Fragment() {
     private lateinit var authorName: String
-    private lateinit var titleName: String
     private lateinit var metaRetriever: MediaMetadataRetriever
     private lateinit var uriPath: String
     private lateinit var uri: Uri
@@ -32,12 +31,12 @@ open class MusicListFragment : Fragment() {
             uri = Uri.parse(uriPath)
             metaRetriever.setDataSource(context, uri)
             authorName = metaRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUM).toString()
-            titleName = metaRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE).toString()
+            val titleName = metaRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE).toString()
             newButton.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
             newButton.text = elm
             newButton.setOnClickListener{
                 requireView().findNavController()
-                    .navigate(MusicListFragmentDirections.actionMusicListFragmentToNowPlayingFragment(elm))
+                    .navigate(MusicListFragmentDirections.actionMusicListFragmentToNowPlayingFragment(elm, titleName))
             }
             musicListContainer.addView(newButton)
         }
--- app/src/main/java/com/ravnnerdery/musicplayer/NowPlayingFragment.kt
+++ app/src/main/java/com/ravnnerdery/musicplayer/NowPlayingFragment.kt
@@ -1,11 +1,13 @@
 package com.ravnnerdery.musicplayer
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TextView
 
 
@@ -18,10 +20,20 @@ class NowPlayingFragment : Fragment() {
     ): View? {
         val binding = inflater.inflate(R.layout.fragment_now_playing, container, false)
         val trackNameTitle = binding.findViewById<TextView>(R.id.titleView)
-        var args = NowPlayingFragmentArgs.fromBundle(requireArguments())
+        val trackPic = binding.findViewById<ImageView>(R.id.albumPic)
+        val args = NowPlayingFragmentArgs.fromBundle(requireArguments())
+        val imgUri = "android.resource://"+activity?.packageName+"/drawable/${args.name}"
+        trackPic.setImageURI(Uri.parse(imgUri))
         name = args.name
-        trackNameTitle.text = name
+        trackNameTitle.text = args.title
         playBtn = binding.findViewById(R.id.play_btn)
+        playBtn.setOnClickListener{
+            if ((activity as MainActivity).getIfPlaying(name)) {
+                (activity as MainActivity).pauseSong(name)
+            } else {
+                (activity as MainActivity).playSong(name)
+            }
+        }
         if (!(activity as MainActivity).getIfPlaying(name)){
             (activity as MainActivity).stopCurrent()
             (activity as MainActivity).playSong(name)
